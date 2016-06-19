fa7f8fa94ef0f6c3dbce4accd06915b4157e5da9
Génoise <idiart.delphine@yahoo.fr>
2016-06-19 23:54:56 +0200

    --

---
+++ www/php/flux_decouverte.php
@@ -0,0 +1,31 @@
+<?php include('config/config.php'); ?>
+
+<?php
+
+/** FONCTION **/
+function selectFlux(PDO $db){
+
+    $sql = "SELECT
+            *
+
+			FROM fichevin
+
+			ORDER BY rand()
+
+			LIMIT 6
+
+          ";
+
+    $req = $db->prepare($sql);
+    $req->execute();
+
+    $tab = $req->fetchAll(PDO::FETCH_ASSOC);
+
+    return $tab;
+
+}
+/** FIN FONCTION **/
+
+/* REQUETE */
+$tab = selectFlux($db);
+echo json_encode($tab);
